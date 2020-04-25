e97caf69e33e9eee901db7345179ba509d37b756
Ryohei Ikegami <iofg2100@gmail.com>
2020-04-25 21:06:52 +0900

    More properties

--- SimpleDALPlugin/Device.swift
+++ SimpleDALPlugin/Device.swift
@@ -15,18 +15,27 @@ class Device: Object {
     let name = "SimpleDALPlugin"
     let deviceUID = "SimpleDALPlugin Device"
     var excludeNonDALAccess: Bool = false
+    var deviceMaster: Int32 = -1
 
     lazy var properties: [Int : Property] = [
         kCMIOObjectPropertyName: Property(name),
         kCMIODevicePropertyDeviceUID: Property(deviceUID),
         kCMIODevicePropertyTransportType: Property(UInt32(kIOAudioDeviceTransportTypeBuiltIn)),
+        kCMIODevicePropertyDeviceIsAlive: Property(UInt32(1)),
+        kCMIODevicePropertyDeviceIsRunning: Property(UInt32(1)),
         kCMIODevicePropertyDeviceIsRunningSomewhere: Property(UInt32(1)),
         kCMIODevicePropertyDeviceCanBeDefaultDevice: Property(UInt32(1)),
+        kCMIODevicePropertyCanProcessAVCCommand: Property(UInt32(0)),
+        kCMIODevicePropertyCanProcessRS422Command: Property(UInt32(0)),
         kCMIODevicePropertyHogMode: Property(Int32(-1)),
         kCMIODevicePropertyStreams: Property { [unowned self] in self.streamID },
         kCMIODevicePropertyExcludeNonDALAccess: Property(
             getter: { [unowned self] () -> UInt32 in self.excludeNonDALAccess ? 1 : 0 },
             setter: { [unowned self] (value: UInt32) -> Void in self.excludeNonDALAccess = value != 0  }
-        )
+        ),
+        kCMIODevicePropertyDeviceMaster: Property(
+            getter: { [unowned self] () -> Int32 in self.deviceMaster },
+            setter: { [unowned self] (value: Int32) -> Void in self.deviceMaster = value  }
+        ),
     ]
 }
