ce110984cc54c245a5c835170ea964cf668a00b6
Ryohei Ikegami <iofg2100@gmail.com>
2020-04-25 14:20:09 +0900

    Implement plugin

--- SimpleDALPlugin/Object.swift
+++ SimpleDALPlugin/Object.swift
@@ -12,7 +12,7 @@ protocol Object: class {
     func hasProperty(address: CMIOObjectPropertyAddress) -> Bool
     func isPropertySettable(address: CMIOObjectPropertyAddress) -> Bool
     func getPropertyDataSize(address: CMIOObjectPropertyAddress) -> UInt32
-    func getPropertyData(address: CMIOObjectPropertyAddress, out dataSize: UInt32, data: UnsafeMutableRawPointer)
+    func getPropertyData(address: CMIOObjectPropertyAddress, dataSize: inout UInt32, data: UnsafeMutableRawPointer)
     func setPropertyData(address: CMIOObjectPropertyAddress, data: UnsafeRawPointer)
 }
 
