c7162ac0e75c35026c3685bc2720d4f6c9cd81a2
Ryohei Ikegami <iofg2100@gmail.com>
2020-04-25 22:42:55 +0900

    Animate square

--- SimpleDALPlugin/Stream.swift
+++ SimpleDALPlugin/Stream.swift
@@ -90,10 +90,14 @@ class Stream: Object {
         let pixelBuffer = CVPixelBuffer.create(size: CGSize(width: width, height: height))
         pixelBuffer?.modifyWithContext { [width, height] context in
             let time = Double(mach_absolute_time()) / Double(1000_000_000)
-            let red = time - round(time)
+            let pos = CGFloat(time - floor(time))
 
-            context.setFillColor(CGColor.init(red: CGFloat(red), green: 0, blue: 0, alpha: 1))
+            context.setFillColor(CGColor.init(red: 1, green: 1, blue: 1, alpha: 1))
             context.fill(CGRect(x: 0, y: 0, width: width, height: height))
+
+            context.setFillColor(CGColor.init(red: 1, green: 0, blue: 0, alpha: 1))
+
+            context.fill(CGRect(x: pos * CGFloat(width), y: 310, width: 100, height: 100))
         }
         return pixelBuffer
     }
