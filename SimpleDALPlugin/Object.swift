5e5c5f50c391daa1a226f1ce30afab2cf36cb5c7
Ryohei Ikegami <iofg2100@gmail.com>
2020-04-25 14:12:38 +0900

    Revert "Add Property"
    This reverts commit 3fc0fbda871483dcae2411fd8d7de722dbe78493.

--- SimpleDALPlugin/Object.swift
+++ SimpleDALPlugin/Object.swift
@@ -8,59 +8,12 @@
 
 import Foundation
 
-protocol PropertyValue {
-    static var dataSize: UInt32 { get }
-    static func fromData(data: UnsafeRawPointer) -> Self
-    func toData(data: UnsafeMutableRawPointer)
-}
-
-protocol PropertyType {
-    var selector: CMIOObjectPropertySelector { get }
-    var isSettable: Bool { get }
-    func getData(dataSize: inout UInt32, data: UnsafeMutableRawPointer)
-    func setData(data: UnsafeRawPointer)
-}
-
-class Property<Element: PropertyValue>: PropertyType {
-    let selector: CMIOObjectPropertySelector
-    private let getter: () -> Element
-    private let setter: ((Element) -> Void)?
-
-    convenience init(selector: CMIOObjectPropertySelector, value: Element) {
-        self.init(selector: selector, getter: { value })
-    }
-
-    init(selector: CMIOObjectPropertySelector, getter: @escaping () -> Element, setter: ((Element) -> Void)? = nil) {
-        self.selector = selector
-        self.getter = getter
-        self.setter = setter
-    }
-
-    var isSettable: Bool {
-        return setter != nil
-    }
-
-    var value: Element {
-        get {
-            return getter()
-        }
-        set {
-            setter?(newValue)
-        }
-    }
-
-    func getData(dataSize: inout UInt32, data: UnsafeMutableRawPointer) {
-        dataSize = Element.dataSize
-        value.toData(data: data)
-    }
-
-    func setData(data: UnsafeRawPointer) {
-        value = Element.fromData(data: data)
-    }
-}
-
 protocol Object: class {
-    var properties: [PropertyType] { get }
+    func hasProperty(address: CMIOObjectPropertyAddress) -> Bool
+    func isPropertySettable(address: CMIOObjectPropertyAddress) -> Bool
+    func getPropertyDataSize(address: CMIOObjectPropertyAddress) -> UInt32
+    func getPropertyData(address: CMIOObjectPropertyAddress, out dataSize: UInt32, data: UnsafeMutableRawPointer)
+    func setPropertyData(address: CMIOObjectPropertyAddress, data: UnsafeRawPointer)
 }
 
 var objects = [CMIOObjectID: Object]()
